d45403d0e68efcc05be80166ea84e6c3cf388502
Andrii Kit <andrii.labyk@gmail.com>
2017-07-12 22:11:31 +0300

    1

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: 'ksd343ksdfsf999034kdsf'
+};
--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
     const email = req.body.email;
@@ -6,12 +13,29 @@ exports.signup = function(req, res, next) {
 
     // See if a user with the given email exists
     User.findOne({ email: email }, function(err, existingUser) {
+        if(err) {
+            return next(err);
+        }
 
+        // If user with this email exist, return an error
+        if(existingUser) {
+            return res.status(422).send({ error: 'Email is in use' });
+        }
     });
 
-    // If a user with the email does exist, return an error
-
     // If a user with email does NOT exist, create and save user record
+    const user = new User({
+        email: email,
+        password: password
+    });
 
-    // Respond to request indicating the user was created
+    user.save(function(err) {
+        // this is callback function
+        if(err) { return next(err); }
+
+        // Respond to request indicating the user was created
+        res.json({ token: tokenForUser(user) });
+    });
+
+
 };
